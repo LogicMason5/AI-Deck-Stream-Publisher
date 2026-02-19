2d37239c173f6e516f6f16347949f4e74b6fd0e9
LogicMason <33540416+LogicMason5@users.noreply.github.com>
2026-02-19 21:35:42 +0900

    Update viewer.py

--- aideck_stream_publisher/viewer.py
+++ aideck_stream_publisher/viewer.py
@@ -1,245 +1,152 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-#     ||          ____  _ __
-#  +------+      / __ )(_) /_______________ _____  ___
-#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
-#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
-#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
-#
-#  Copyright (C) 2021 Bitcraze AB
-#
-#  AI-deck demo
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  You should have received a copy of the GNU General Public License along with
-#  this program; if not, write to the Free Software Foundation, Inc., 51
-#  Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-
-###================================
-# Originally taken from the Bitcraze AIDeck GAP8 examples: https://github.com/bitcraze/aideck-gap8-examples
-
-# Modified by Miguel Granero. 2022
-#   - ROS Package created
-#   - Modification to functionality to adapt it to a better ros usage
-#   - Image publisher
-#   - Color correction
-###================================
-
-
-import argparse
-
+import socket
+import struct
 import time
+import os
 
-import socket,os,struct, time
 import numpy as np
-
 import cv2
 
-import 
-
-from rclpy.node import 
-
+import rclpy
+from rclpy.node import Node
 from sensor_msgs.msg import Image
-
 from cv_bridge import CvBridge
 
-def colorBalance(img):
-    '''
-    Function to color balance wrt a white object
-    '''
-    mean = [0,0,0]
-    factor = [1,1,1]
-    img_out = img.copy()
-    for c in range(3):
-        mean[c] = np.mean(img[:,:,c].flatten())
-        factor[c] = 255.0/mean[c]
-        img_out[:,:,c] = np.clip(img_out[:,:,c]*factor[c],0,255)
 
-    print("Factors: {}".format(factor))
-    return img_out, factor
+class AIDeckPublisher(Node):
 
-def colorBalanceStretch(img, perc = 0.05):
-    '''
-    Similar to the above, but only using min and max in the 0.05 percentile.
-    '''
-    factor = [1,1,1]
-    img_out = img.copy()
-    for c in range(3):
-        mi, ma = (np.percentile(img[:,:,c], perc), np.percentile(img[:,:,c],100.0-perc))
-        factor[c] = 255.0/(ma-mi)
-        img_out[:,:,c] = np.clip(img_out[:,:,c]*factor[c],0,255)
-
-    print("Factors: {}".format(factor))
-    return img_out, factor
-    
-
-class aideckPublisher(Node):
-    '''
-    Class for the publisher
-    '''
     def __init__(self):
         super().__init__('aideck_stream_publisher')
 
-        # Args for setting IP/port of AI-deck. Default settings are for when
-        # AI-deck is in AP mode.
-        self.declare_parameter('ip','192.168.4.1')
-        deck_ip = self.get_parameter('ip').value
-        self.declare_parameter('port',5000)
-        deck_port = self.get_parameter('port').value
-        self.declare_parameter('save_flag',False)
-        self.declare_parameter('show_flag',False)
-        
-        self.factors = [1.8648577393897736, 1.2606252586922309, 1.4528872589128194]
+        # Parameters
+        self.declare_parameter('ip', '192.168.4.1')
+        self.declare_parameter('port', 5000)
+        self.declare_parameter('save_flag', False)
+        self.declare_parameter('show_flag', False)
 
+        self.deck_ip = self.get_parameter('ip').value
+        self.deck_port = self.get_parameter('port').value
+        self.save_flag = self.get_parameter('save_flag').value
+        self.show_flag = self.get_parameter('show_flag').value
+
+        # Precomputed color correction factors
+        self.factors = np.array([1.86, 1.26, 1.45], dtype=np.float32)
+
+        # ROS publisher
         self.publisher_ = self.create_publisher(Image, 'aideck/image', 10)
-        timer_period = 0.1  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
-
-        print("Connecting to socket on {}:{}...".format(deck_ip, deck_port))
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((deck_ip, deck_port))
-        print("Socket connected")
-
         self.br = CvBridge()
 
-        self.start = time.time()
-        self.count = 0
+        # Performance tracking
+        self.start_time = time.time()
+        self.frame_count = 0
 
-    def colorCorrectBayer(self,img_, factors=[1,1,1]):
-        '''
-        Color correction for the RGB Camera. It has a sensor with a Bayer pattern, which has
-        more green cells than blue and red, so if the image is not treated, it will have  a
-        green-ish look.
-        '''
-        # TODO: Apply an actual color correction without luminosity loss. -> histogram level
-        # This is just an approximation
-        img = img_.copy()
-        for i in range(3):
-            img[:,:,i] = np.clip(img[:,:,i]*factors[i],0,255)
-        return img
+        # Socket setup
+        self.get_logger().info(f"Connecting to {self.deck_ip}:{self.deck_port}")
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.deck_ip, self.deck_port))
+        self.client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.get_logger().info("Socket connected")
 
-    def rx_bytes(self,size, client_socket):
-        '''
-        Read N bytes from the socket
-        '''
-        data = bytearray()
-        while len(data) < size:
-            data.extend(client_socket.recv(size-len(data)))
-        return data
+        # Start streaming loop
+        self.create_timer(0.0, self.stream_loop)  # Run as fast as possible
 
+    # ===============================
+    # Efficient socket reader
+    # ===============================
+    def rx_bytes(self, size):
+        buffer = bytearray(size)
+        view = memoryview(buffer)
+        received = 0
+        while received < size:
+            n = self.client_socket.recv_into(view[received:])
+            if n == 0:
+                raise RuntimeError("Socket connection lost")
+            received += n
+        return buffer
 
-    def getImage(self, client_socket):
-        '''
-        Function to receive an image from the socket
-        '''
-        imgdata = None
-        data_buffer = bytearray()
-        # First get the info
-        packetInfoRaw = self.rx_bytes(4, client_socket)
-        #print(packetInfoRaw)
-        [length, routing, function] = struct.unpack('<HBB', packetInfoRaw)
-        #print("Length is {}".format(length))
-        #print("Route is 0x{:02X}->0x{:02X}".format(routing & 0xF, routing >> 4))
-        #print("Function is 0x{:02X}".format(function))
+    # ===============================
+    # Fast color correction
+    # ===============================
+    def color_correct(self, img):
+        img = img.astype(np.float32)
+        img *= self.factors
+        np.clip(img, 0, 255, out=img)
+        return img.astype(np.uint8)
 
-        imgHeader = self.rx_bytes(length - 2, client_socket)
-        #print(imgHeader)
-        #print("Length of data is {}".format(len(imgHeader)))
-        [magic, width, height, depth, format, size] = struct.unpack('<BHHBBI', imgHeader)
+    # ===============================
+    # Image Receiver
+    # ===============================
+    def get_image(self):
 
-        imgs = None
+        packet_info = self.rx_bytes(4)
+        length, routing, function = struct.unpack('<HBB', packet_info)
 
-        if magic == 0xBC:
-            #print("Magic is good")
-            #print("Resolution is {}x{} with depth of {} byte(s)".format(width, height, depth))
-            #print("Image format is {}".format(format))
-            #print("Image size is {} bytes".format(size))
+        img_header = self.rx_bytes(length - 2)
+        magic, width, height, depth, fmt, size = struct.unpack('<BHHBBI', img_header)
 
-            # Now we start rx the image, this will be split up in packages of some size
-            imgStream = bytearray()
+        if magic != 0xBC:
+            return None, None
 
-            while len(imgStream) < size:
-                packetInfoRaw = self.rx_bytes(4, client_socket)
-                [length, dst, src] = struct.unpack('<HBB', packetInfoRaw)
-                #print("Chunk size is {} ({:02X}->{:02X})".format(length, src, dst))
-                chunk = self.rx_bytes(length - 2, client_socket)
-                imgStream.extend(chunk)
-            
-            self.count = self.count + 1
-            meanTimePerImage = (time.time()-self.start) / self.count
+        img_stream = bytearray(size)
+        view = memoryview(img_stream)
+        received = 0
 
-            # TODO: CHange to debug and rclpy
-            #print("{}".format(meanTimePerImage))
-            #print("{}".format(1/meanTimePerImage))
+        while received < size:
+            packet_info = self.rx_bytes(4)
+            length, dst, src = struct.unpack('<HBB', packet_info)
+            chunk = self.rx_bytes(length - 2)
+            view[received:received+len(chunk)] = chunk
+            received += len(chunk)
 
-            if format == 0:
-                bayer_img = np.frombuffer(imgStream, dtype=np.uint8)   
-                bayer_img.shape = (244, 324)
-                color_img = cv2.cvtColor(bayer_img, cv2.COLOR_BayerBG2BGR)
+        if fmt == 0:
+            bayer = np.frombuffer(img_stream, dtype=np.uint8)
+            bayer = bayer.reshape((244, 324))
+            color = cv2.cvtColor(bayer, cv2.COLOR_BayerBG2BGR)
+            return fmt, color
+        else:
+            nparr = np.frombuffer(img_stream, np.uint8)
+            decoded = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+            return fmt, decoded
 
-                k = cv2.waitKey(1)
-                if k == ord('b'):
-                    _,self.factors = colorBalance(color_img)
-                
-                if self.get_parameter('save_flag').value:
-                    cv2.imwrite(f"stream_out/raw/img_{self.count:06d}.png", bayer_img)
-                    cv2.imwrite(f"stream_out/debayer/img_{self.count:06d}.png", color_img)
-                if self.get_parameter('show_flag').value:
-                    cv2.imshow('Raw', bayer_img)
-                    cv2.imshow('Color', self.colorCorrectBayer(color_img,self.factors))
-                    cv2.waitKey(1)
-                imgs = [bayer_img,color_img]
-            else:
-                if self.get_parameter('save_flag').value:
-                    with open("img.jpeg", "wb") as f:
-                        f.write(imgStream)
-                nparr = np.frombuffer(imgStream, np.uint8)
-                decoded = cv2.imdecode(nparr,cv2.IMREAD_UNCHANGED)
-                if self.get_parameter('show_flag').value:
-                    cv2.imshow('JPEG', decoded)
-                    cv2.waitKey(1)
-                imgs = [decoded]
+    # ===============================
+    # Streaming Loop
+    # ===============================
+    def stream_loop(self):
 
-        return format,imgs
+        fmt, img = self.get_image()
 
-    def timer_callback(self):
-        '''
-        The class works with a timer. This is the main action in each loop.
-        '''
-        msg = Image()
-        format, imgs = self.getImage(self.client_socket)
+        if img is None:
+            return
 
-        if imgs is not None and format == 0:
-            #self.get_logger().info('Publishing: "%s"' % self.i)
-            img = imgs[-1]
-            msg = self.br.cv2_to_imgmsg(self.colorCorrectBayer(img,self.factors))
-            msg.header.stamp = self.get_clock().now().to_msg()
-            self.publisher_.publish(msg)
-            self.i += 1
+        if fmt == 0:
+            img = self.color_correct(img)
 
+        # Publish
+        msg = self.br.cv2_to_imgmsg(img, encoding="bgr8")
+        msg.header.stamp = self.get_clock().now().to_msg()
+        self.publisher_.publish(msg)
+
+        # Optional display
+        if self.show_flag:
+            cv2.imshow("AIDeck", img)
+            cv2.waitKey(1)
+
+        # FPS calculation
+        self.frame_count += 1
+        if self.frame_count % 30 == 0:
+            elapsed = time.time() - self.start_time
+            fps = self.frame_count / elapsed
+            self.get_logger().info(f"FPS: {fps:.2f}")
 
 
 def main(args=None):
-
     rclpy.init(args=args)
-    stream_pub = aideckPublisher()
-    rclpy.spin(stream_pub)
-
-    stream_pub.destroy_node()
-
+    node = AIDeckPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
-
 
 
 if __name__ == '__main__':
