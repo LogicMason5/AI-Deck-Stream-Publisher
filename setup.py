59a27b69b519c681d8736e38e12c7e58b8f336b9
LogicMason <33540416+LogicMason5@users.noreply.github.com>
2026-03-04 20:55:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,39 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from glob import glob
 import os
 
-package_name = 'aideck_stream_publisher'
+package_name = "aideck_stream_publisher"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=[package_name],
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.py')),
+        # Required for ament index
+        (
+            "share/ament_index/resource_index/packages",
+            [f"resource/{package_name}"],
+        ),
+        # Package manifest
+        (f"share/{package_name}", ["package.xml"]),
+        # Launch files
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*.py")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        "setuptools",
+    ],
     zip_safe=True,
-    maintainer='miguel',
-    maintainer_email='miguelgranero99@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="Miguel Granero",
+    maintainer_email="miguelgranero99@gmail.com",
+    description="ROS2 package for streaming and viewing AI-Deck video feed.",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'viewer = aideck_stream_publisher.viewer:main'
+        "console_scripts": [
+            "viewer = aideck_stream_publisher.viewer:main",
         ],
     },
 )
